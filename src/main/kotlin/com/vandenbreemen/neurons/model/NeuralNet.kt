06ecf07487d0a0a0658770966bddb4b3ef36c609
Kevin <kevin.vandenbreemen@gmail.com>
2025-03-15 13:09:59 -0700

    Make the connection strengths random

--- src/main/kotlin/com/vandenbreemen/neurons/model/NeuralNet.kt
+++ src/main/kotlin/com/vandenbreemen/neurons/model/NeuralNet.kt
@@ -7,15 +7,35 @@ class NeuralNet(val rows: Int, val cols: Int) {
         for (i in 0 until rows) {
             for (j in 0 until cols) {
                 val neuron = grid[i][j]
-                if (i > 0) neuron.connect(grid[i - 1][j]) // Connect to the neuron above
-                if (i < rows - 1) neuron.connect(grid[i + 1][j]) // Connect to the neuron below
-                if (j > 0) neuron.connect(grid[i][j - 1]) // Connect to the neuron to the left
-                if (j < cols - 1) neuron.connect(grid[i][j + 1]) // Connect to the neuron to the right
-                if (i > 0 && j > 0) neuron.connect(grid[i - 1][j - 1]) // Connect to the neuron above-left
-                if (i > 0 && j < cols - 1) neuron.connect(grid[i - 1][j + 1]) // Connect to the neuron above-right
-                if (i < rows - 1 && j > 0) neuron.connect(grid[i + 1][j - 1]) // Connect to the neuron below-left
-                if (i < rows - 1 && j < cols - 1) neuron.connect(grid[i + 1][j + 1]) // Connect to the neuron below-right
-
+                if (i > 0) neuron.connect(grid[i - 1][j], (-100..100).random() / 100.0) // Connect to the neuron above
+                if (i < rows - 1) neuron.connect(
+                    grid[i + 1][j],
+                    (-100..100).random() / 100.0
+                ) // Connect to the neuron below
+                if (j > 0) neuron.connect(
+                    grid[i][j - 1],
+                    (-100..100).random() / 100.0
+                ) // Connect to the neuron to the left
+                if (j < cols - 1) neuron.connect(
+                    grid[i][j + 1],
+                    (-100..100).random() / 100.0
+                ) // Connect to the neuron to the right
+                if (i > 0 && j > 0) neuron.connect(
+                    grid[i - 1][j - 1],
+                    (-100..100).random() / 100.0
+                ) // Connect to the neuron above-left
+                if (i > 0 && j < cols - 1) neuron.connect(
+                    grid[i - 1][j + 1],
+                    (-100..100).random() / 100.0
+                ) // Connect to the neuron above-right
+                if (i < rows - 1 && j > 0) neuron.connect(
+                    grid[i + 1][j - 1],
+                    (-100..100).random() / 100.0
+                ) // Connect to the neuron below-left
+                if (i < rows - 1 && j < cols - 1) neuron.connect(
+                    grid[i + 1][j + 1],
+                    (-100..100).random() / 100.0
+                ) // Connect to the neuron below-right
             }
         }
     }
