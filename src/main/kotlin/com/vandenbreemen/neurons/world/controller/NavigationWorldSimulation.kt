8ae31ed3fb30458ab70d430f5e0e2418830bed2d
Kevin <kevin.vandenbreemen@gmail.com>
2025-04-09 18:31:24 -0700

    Make this easier to parameterize

--- src/main/kotlin/com/vandenbreemen/neurons/world/controller/NavigationWorldSimulation.kt
+++ src/main/kotlin/com/vandenbreemen/neurons/world/controller/NavigationWorldSimulation.kt
@@ -5,6 +5,7 @@ import com.vandenbreemen.neurons.world.model.World
 
 class NavigationWorldSimulation(
     world: World = World(),
+    private val maxMovementDelta: Int = 2
 ) : WorldSimulation(world) {
 
     override fun doAgentSetup(agent: NeuralAgent) {
@@ -16,88 +17,81 @@ class NavigationWorldSimulation(
         // North
         agent.findMotorNeurons { idByte -> idByte in 0x00..0x1F }.forEach { neuron ->
             agent.addNeuronAction(neuron) {
-                if (neuron.activation > 0.5) {
-                    val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
-                    val newPos = currentPos.copy(y = currentPos.y - 1)
-                    setAgentPosition(agent, newPos)
-                }
+                val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
+                // Map activation from [0, 1] to [0, maxMovementDelta]
+                val delta = (neuron.activation * maxMovementDelta).toInt()
+                val newPos = currentPos.copy(y = currentPos.y - delta)
+                setAgentPosition(agent, newPos)
             }
         }
 
         // Northeast
         agent.findMotorNeurons { idByte -> idByte in 0x20..0x3F }.forEach { neuron ->
             agent.addNeuronAction(neuron) {
-                if (neuron.activation > 0.5) {
-                    val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
-                    val newPos = currentPos.copy(x = currentPos.x + 1, y = currentPos.y - 1)
-                    setAgentPosition(agent, newPos)
-                }
+                val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
+                val delta = (neuron.activation * maxMovementDelta).toInt()
+                val newPos = currentPos.copy(x = currentPos.x + delta, y = currentPos.y - delta)
+                setAgentPosition(agent, newPos)
             }
         }
 
         // East
         agent.findMotorNeurons { idByte -> idByte in 0x40..0x5F }.forEach { neuron ->
             agent.addNeuronAction(neuron) {
-                if (neuron.activation > 0.5) {
-                    val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
-                    val newPos = currentPos.copy(x = currentPos.x + 1)
-                    setAgentPosition(agent, newPos)
-                }
+                val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
+                val delta = (neuron.activation * maxMovementDelta).toInt()
+                val newPos = currentPos.copy(x = currentPos.x + delta)
+                setAgentPosition(agent, newPos)
             }
         }
 
         // Southeast
         agent.findMotorNeurons { idByte -> idByte in 0x60..0x7F }.forEach { neuron ->
             agent.addNeuronAction(neuron) {
-                if (neuron.activation > 0.5) {
-                    val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
-                    val newPos = currentPos.copy(x = currentPos.x + 1, y = currentPos.y + 1)
-                    setAgentPosition(agent, newPos)
-                }
+                val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
+                val delta = (neuron.activation * maxMovementDelta).toInt()
+                val newPos = currentPos.copy(x = currentPos.x + delta, y = currentPos.y + delta)
+                setAgentPosition(agent, newPos)
             }
         }
 
         // South
         agent.findMotorNeurons { idByte -> idByte in 0x80..0x9F }.forEach { neuron ->
             agent.addNeuronAction(neuron) {
-                if (neuron.activation > 0.5) {
-                    val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
-                    val newPos = currentPos.copy(y = currentPos.y + 1)
-                    setAgentPosition(agent, newPos)
-                }
+                val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
+                val delta = (neuron.activation * maxMovementDelta).toInt()
+                val newPos = currentPos.copy(y = currentPos.y + delta)
+                setAgentPosition(agent, newPos)
             }
         }
 
         // Southwest
         agent.findMotorNeurons { idByte -> idByte in 0xA0..0xBF }.forEach { neuron ->
             agent.addNeuronAction(neuron) {
-                if (neuron.activation > 0.5) {
-                    val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
-                    val newPos = currentPos.copy(x = currentPos.x - 1, y = currentPos.y + 1)
-                    setAgentPosition(agent, newPos)
-                }
+                val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
+                val delta = (neuron.activation * maxMovementDelta).toInt()
+                val newPos = currentPos.copy(x = currentPos.x - delta, y = currentPos.y + delta)
+                setAgentPosition(agent, newPos)
             }
         }
 
         // West
         agent.findMotorNeurons { idByte -> idByte in 0xC0..0xDF }.forEach { neuron ->
             agent.addNeuronAction(neuron) {
-                if (neuron.activation > 0.5) {
-                    val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
-                    val newPos = currentPos.copy(x = currentPos.x - 1)
-                    setAgentPosition(agent, newPos)
-                }
+                val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
+                val delta = (neuron.activation * maxMovementDelta).toInt()
+                val newPos = currentPos.copy(x = currentPos.x - delta)
+                setAgentPosition(agent, newPos)
             }
         }
 
         // Northwest
         agent.findMotorNeurons { idByte -> idByte in 0xE0..0xFF }.forEach { neuron ->
             agent.addNeuronAction(neuron) {
-                if (neuron.activation > 0.5) {
-                    val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
-                    val newPos = currentPos.copy(x = currentPos.x - 1, y = currentPos.y - 1)
-                    setAgentPosition(agent, newPos)
-                }
+                val currentPos = getAgentPosition(agent) ?: return@addNeuronAction
+                val delta = (neuron.activation * maxMovementDelta).toInt()
+                val newPos = currentPos.copy(x = currentPos.x - delta, y = currentPos.y - delta)
+                setAgentPosition(agent, newPos)
             }
         }
     }
