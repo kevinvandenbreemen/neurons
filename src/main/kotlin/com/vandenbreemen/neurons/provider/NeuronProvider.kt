756d2efd81ba10a72e9e11aeac92ff32d3198b50
Kevin <kevin.vandenbreemen@gmail.com>
2025-06-12 18:37:12 -0700

    Get rid of un-used random neuron provider

--- src/main/kotlin/com/vandenbreemen/neurons/provider/NeuronProvider.kt
+++ src/main/kotlin/com/vandenbreemen/neurons/provider/NeuronProvider.kt
@@ -2,7 +2,6 @@ package com.vandenbreemen.neurons.provider
 
 import com.vandenbreemen.neurons.model.ConnectionWeightCalculator
 import com.vandenbreemen.neurons.model.DefaultConnectionWeightCalculator
-import com.vandenbreemen.neurons.model.FixedWeightNeuron
 import com.vandenbreemen.neurons.model.Neuron
 
 interface NeuronProvider {
@@ -15,31 +14,6 @@ class DefaultNeuronProvider(private val weightCalculator: ConnectionWeightCalcul
 
     override fun getNeuron(): Neuron {
         return Neuron(weightCalculator)
-    }
-
-}
-
-class RandomNeuronProvider(
-    private val fixedWeightNeuronPercentage: Double = 0.0,
-    private val weightCalculator: ConnectionWeightCalculator = DefaultConnectionWeightCalculator
-) : NeuronProvider {
-
-    init {
-        require(fixedWeightNeuronPercentage <= 1.0) { "Percentage must be 1.0 or less, but is $fixedWeightNeuronPercentage" }
-        require(fixedWeightNeuronPercentage >= 0) {
-            "Percentage must be between 0 and 1"
-        }
-    }
-
-    private val regularNeuronPercentage: Double
-        get() = 1.0 - fixedWeightNeuronPercentage
-
-    override fun getNeuron(): Neuron {
-        val random = Math.random()
-        return when {
-            random < regularNeuronPercentage -> Neuron(weightCalculator)
-            else -> FixedWeightNeuron(weightCalculator)
-        }
     }
 
 }
